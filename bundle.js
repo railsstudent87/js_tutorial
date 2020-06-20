943202f3ab0330de86a0c4a01003710d9d7a213d
Rails Student <railsstudent@pm.me>
2020-06-20 12:47:31 +0000

    Finish palindrome form

--- bundle.js
+++ bundle.js
@@ -1,14 +1,36 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 let Phrase = require("railsstudent87-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
-
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`);
+function palindromeTester(event) {
+  event.preventDefault(); //Since our “server” is just a static web page, we can’t handle such a submission, so we need to prevent this default behavior. event is a special object that JavaScript provides for just this sort of case
+  // let string = prompt("Please enter a string for palindrome testing:");
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
+  
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome!`;
+  }
 }
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   let button = document.querySelector("#palindromeTester");
+  
+//   button.addEventListener("click", function() {
+//     palindromeTester();
+//   });
+//   // button.addEventListener("click", palindromeTester); would work but we’ve used an anonymous function 
+//   //                                                     instead to emphasize the general case where there 
+//   //                                                     might be more than one line.
+// })
+
+document.addEventListener("DOMContentLoaded", function() {
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function(event) {
+    palindromeTester(event);
+  });
+})
 },{"railsstudent87-palindrome":2}],2:[function(require,module,exports){
 module.exports = Phrase;
 
@@ -42,7 +64,11 @@ function Phrase(content) {
   
   // Returns true for a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    return this.processedContent() === this.processedContent().reverse();
+    if (this.processedContent()) {
+      return this.processedContent() === this.processedContent().reverse();
+    } else {
+      return false;
+    }
   }
   
   this.louder = function louder() {
@@ -51,7 +77,8 @@ function Phrase(content) {
   
   this.letters = function letters() {
     // return Array.from(this.content).filter(ch => ch.match(/[a-z]/i)).join("");
-    return (this.content.match(/[a-z]/gi) || []).join("");
+    const lettersRegEx = /[a-z]/gi;
+    return (this.content.match(lettersRegEx) || []).join("");
   }
   
 }
--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 let Phrase = require("railsstudent87-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
-
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`);
+function palindromeTester(event) {
+  event.preventDefault(); //Since our “server” is just a static web page, we can’t handle such a submission, so we need to prevent this default behavior. event is a special object that JavaScript provides for just this sort of case
+  // let string = prompt("Please enter a string for palindrome testing:");
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
+  
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome!`;
+  }
 }
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   let button = document.querySelector("#palindromeTester");
+  
+//   button.addEventListener("click", function() {
+//     palindromeTester();
+//   });
+//   // button.addEventListener("click", palindromeTester); would work but we’ve used an anonymous function 
+//   //                                                     instead to emphasize the general case where there 
+//   //                                                     might be more than one line.
+// })
+
+document.addEventListener("DOMContentLoaded", function() {
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function(event) {
+    palindromeTester(event);
+  });
+})
